793a72fb674f48be7796ba684ebe4a727f57f156
mateus.macedo <mhbassomacedo@gmail.com>
2017-06-22 15:17:53 -0300

    Colocando ação nos botões de voltar nas activity Main e Inserir
    Signed-off-by: mateus.macedo <mhbassomacedo@gmail.com>

--- app/src/main/java/com/example/mateusmacedo/projetofinalmateus/InserirActivity.java
+++ app/src/main/java/com/example/mateusmacedo/projetofinalmateus/InserirActivity.java
@@ -1,13 +1,27 @@
 package com.example.mateusmacedo.projetofinalmateus;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class InserirActivity extends AppCompatActivity {
+
+    Button buttonVoltarInicio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir);
+
+        buttonVoltarInicio=(Button)findViewById(R.id.btnvoltarinicio);
+        buttonVoltarInicio.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
